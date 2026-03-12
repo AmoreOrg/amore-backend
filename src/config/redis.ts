df372040994bff8440a40e5228fbca75c50cfc16
Talentraze <talentraze1@gmail.com>
2026-03-12 09:47:49 +0530

    fgchdf

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -5,12 +5,16 @@ import Redis from 'ioredis';
 import { config } from '../config';
 import logger from '../utils/logger';
 
-export const redis = new Redis({
-  host: config.redis.host,
-  port: config.redis.port,
-  password: config.redis.password,
-  maxRetriesPerRequest: null, // required by BullMQ
-});
+const redisUrl = process.env.REDIS_URL;
+
+export const redis = redisUrl
+  ? new Redis(redisUrl, { maxRetriesPerRequest: null })
+  : new Redis({
+      host: config.redis.host,
+      port: config.redis.port,
+      password: config.redis.password,
+      maxRetriesPerRequest: null, // required by BullMQ
+    });
 
 redis.on('connect', () => logger.info('Redis connected'));
 redis.on('error', (err) => logger.error('Redis error:', err));
